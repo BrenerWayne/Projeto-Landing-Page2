ab513fc6ddab0830f817a58530dc219633161054
Brener <Brenerwayne@gmail.com>
2023-05-21 21:36:46 -0300

    Versão Inicial

---
+++ Projeto-Landingpage/index.js
@@ -0,0 +1,21 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var bruna = window.document.getElementById("bruna")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    bruna.style ="display:none"
+    samantha.style ="display:flex"
+    setaDireita.style ="display:none"
+    setaEsquerda.style ="display:flex; margin-top 50px"
+}
+
+function RolarParaEsquerda() {
+    bruna.style ="display:flex"
+    samantha.style ="diplay:none"
+    setaDireita.style = "display:flex; margin-top 50px"
+    setaEsquerda.style = "diplay:none"
+
+}
+
